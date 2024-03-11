aba933df0f6565765d1077aad4d74eefe1060521
FloRul <rumiel.florian@gmail.com>
2024-03-11 10:14:24 -0400

    Update Terraform provider versions and fix API integration

--- modules/api/integrations.tf
+++ modules/api/integrations.tf
@@ -6,7 +6,7 @@ module "integrations" {
   aws_region           = var.aws_region
   lambda_arn           = each.value.lambda_arn
   lambda_name          = each.value.lambda_name
-  api_root_resource_id = aws_api_gateway_resource.api_gateway_resource.id
+  api_root_resource_id = aws_api_gateway_rest_api.this.root_resource_id
   api_path_part        = each.value.api_path_part
   http_method          = each.value.http_method
 }
