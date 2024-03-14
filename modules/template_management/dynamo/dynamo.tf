68c742e23fc014eec9f1d6b71a8c37a8be2181b3
FloRul <rumiel.florian@gmail.com>
2024-03-13 21:12:32 -0400

    Update hash key and attribute names in DynamoDB table and lambda function

--- modules/template_management/dynamo/dynamo.tf
+++ modules/template_management/dynamo/dynamo.tf
@@ -2,12 +2,12 @@ module "template_index_table" {
   source = "terraform-aws-modules/dynamodb-table/aws"
 
   name         = "${var.project_name}-templates-${var.environment}"
-  hash_key     = "PK"
+  hash_key     = "id"
   billing_mode = "PAY_PER_REQUEST"
 
   attributes = [
     {
-      name = "PK"
+      name = "id"
       type = "S"
     },
   ]
