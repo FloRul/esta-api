02ca62303bd93b971b9c9c1d2fffde9a105132fa
FloRul <rumiel.florian@gmail.com>
2024-03-15 16:40:19 -0400

    Replace PyPDF2 with pypdf in index.py

--- modules/ingestion/parsers/pypdf/src/index.py
+++ modules/ingestion/parsers/pypdf/src/index.py
@@ -1,4 +1,4 @@
-import PyPDF2
+import pypdf as pypdf
 import boto3
 from aws_lambda_powertools import Logger, Metrics, Tracer
 import os
@@ -22,7 +22,7 @@ def lambda_handler(event, context):
         obj = s3.get_object(Bucket=bucket, Key=key)
         pdf_file_content = obj["Body"].read()
 
-        pdf_file = PyPDF2.PdfFileReader(io.BytesIO(pdf_file_content))
+        pdf_file = pypdf.PdfReader(io.BytesIO(pdf_file_content))
         text = ""
         for page in range(pdf_file.getNumPages()):
             text += pdf_file.getPage(page).extractText()
