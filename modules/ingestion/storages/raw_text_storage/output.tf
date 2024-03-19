57dcf438cb308e42f4da1301da94caa4a1b3356a
FloRul <rumiel.florian@gmail.com>
2024-03-19 16:28:14 -0400

    Add bucket_arn output to raw_text_storage module

--- modules/ingestion/storages/raw_text_storage/output.tf
+++ modules/ingestion/storages/raw_text_storage/output.tf
@@ -2,6 +2,10 @@ output "bucket_id" {
   value = module.raw_text_storage.s3_bucket_id
 }
 
+output "bucket_arn" {
+  value = module.raw_text_storage.s3_bucket_arn
+}
+
 output "parsing_queue_arn" {
   value = aws_sqs_queue.parsing_queue.arn
 }
