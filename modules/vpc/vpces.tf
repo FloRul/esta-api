5d7a13a49b8772f9b32acfb3a4682ad99409146b
FloRul <rumiel.florian@gmail.com>
2024-03-19 22:06:23 -0400

    Update function to handle edge cases

--- modules/vpc/vpces.tf
+++ modules/vpc/vpces.tf
@@ -62,8 +62,7 @@ resource "aws_vpc_endpoint" "dynamo_db" {
 resource "aws_vpc_endpoint" "s3" {
   vpc_id              = module.vpc.vpc_id
   service_name        = "com.amazonaws.${var.region}.s3"
-  vpc_endpoint_type   = "Interface"
-  security_group_ids  = [aws_security_group.s3_sg.id]
-  subnet_ids          = module.vpc.public_subnets
+  vpc_endpoint_type   = "Gateway"
+  route_table_ids     = module.vpc.public_route_table_ids
   private_dns_enabled = true
 }
