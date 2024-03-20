c9d83aa0c1d4dc38b2b08f1e34d2150fc885105a
FloRul <rumiel.florian@gmail.com>
2024-03-20 10:53:49 -0400

    Update AWS VPC endpoint for S3

--- modules/vpc/vpces.tf
+++ modules/vpc/vpces.tf
@@ -59,7 +59,7 @@ resource "aws_vpc_endpoint" "dynamo_db" {
   route_table_ids   = module.vpc.public_route_table_ids
 }
 
-resource "aws_vpc_endpoint" "dynamo_db" {
+resource "aws_vpc_endpoint" "s3" {
   vpc_id            = module.vpc.vpc_id
   service_name      = "com.amazonaws.${var.region}.s3"
   vpc_endpoint_type = "Gateway"
