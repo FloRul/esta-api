39a66c8cfc67fa5cf994986dcbd68a62f2143ee6
FloRul <rumiel.florian@gmail.com>
2024-03-21 13:14:26 -0400

    Update sg_rules.tf to allow egress traffic from bastion_sg to 75.152.59.245/32 on port 443

--- modules/vpc/sg_rules.tf
+++ modules/vpc/sg_rules.tf
@@ -25,14 +25,14 @@ resource "aws_security_group_rule" "database_sg_ingress_lambda" {
   security_group_id        = aws_security_group.database_sg.id
 }
 
-# resource "aws_security_group_rule" "bastion_sg_egress_ssm" {
-#   type              = "egress"
-#   from_port         = 443
-#   to_port           = 443
-#   protocol          = "tcp"
-#   cidr_blocks       = ["75.152.59.245/32"]
-#   security_group_id = aws_security_group.bastion_sg.id
-# }
+resource "aws_security_group_rule" "bastion_sg_egress_ssm" {
+  type              = "egress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["75.152.59.245/32"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
 
 resource "aws_security_group_rule" "bastion_sg_ingress_ssm" {
   type              = "ingress"
