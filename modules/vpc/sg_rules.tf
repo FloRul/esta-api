9d45c458d68e14dd51d997de89dfa8d6ccb7c58b
FloRul <rumiel.florian@gmail.com>
2024-03-21 10:17:26 -0400

    Remove duplicate security group rule for egress traffic

--- modules/vpc/sg_rules.tf
+++ modules/vpc/sg_rules.tf
@@ -52,15 +52,6 @@ resource "aws_security_group_rule" "bastion_sg_egress_ssm" {
   security_group_id = aws_security_group.bastion_sg.id
 }
 
-resource "aws_security_group_rule" "bastion_sg_egress_ssm" {
-  type                     = "egress"
-  from_port                = 443
-  to_port                  = 443
-  protocol                 = "tcp"
-  source_security_group_id = aws_security_group.database_sg.id
-  security_group_id        = aws_security_group.bastion_sg.id
-}
-
 resource "aws_security_group_rule" "lambda_sg_egress_rds" {
   type                     = "egress"
   from_port                = 0
