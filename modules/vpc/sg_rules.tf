ce2602a81b7a7309893adfa0f72cf714481df81b
FloRul <rumiel.florian@gmail.com>
2024-03-21 10:28:35 -0400

    Remove unused security group rules for bastion instance

--- modules/vpc/sg_rules.tf
+++ modules/vpc/sg_rules.tf
@@ -25,24 +25,6 @@ resource "aws_security_group_rule" "database_sg_ingress_lambda" {
   security_group_id        = aws_security_group.database_sg.id
 }
 
-resource "aws_security_group_rule" "bastion_sg_ingress_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bastion_sg.id
-}
-
-resource "aws_security_group_rule" "bastion_sg_ingress_ssm" {
-  type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.bastion_sg.id
-}
-
 resource "aws_security_group_rule" "bastion_sg_egress_ssm" {
   type              = "egress"
   from_port         = 443
