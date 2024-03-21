aeb8dfee14594782fd8eb29eccc522ed1a64abe0
FloRul <rumiel.florian@gmail.com>
2024-03-21 13:26:56 -0400

    Update security group rules for bastion SG ingress and egress

--- modules/vpc/sg_rules.tf
+++ modules/vpc/sg_rules.tf
@@ -27,35 +27,17 @@ resource "aws_security_group_rule" "database_sg_ingress_lambda" {
 
 resource "aws_security_group_rule" "bastion_sg_egress_ssm" {
   type              = "egress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   cidr_blocks       = ["75.152.59.245/32"]
   security_group_id = aws_security_group.bastion_sg.id
 }
 
 resource "aws_security_group_rule" "bastion_sg_ingress_ssm" {
   type              = "ingress"
-  from_port         = 443
-  to_port           = 443
-  protocol          = "tcp"
-  cidr_blocks       = ["75.152.59.245/32"]
-  security_group_id = aws_security_group.bastion_sg.id
-}
-
-resource "aws_security_group_rule" "bastion_sg_egress_ssh" {
-  type              = "egress"
-  from_port         = 22
-  to_port           = 22
-  protocol          = "-1"
-  cidr_blocks       = ["75.152.59.245/32"]
-  security_group_id = aws_security_group.bastion_sg.id
-}
-
-resource "aws_security_group_rule" "bastion_sg_ingress_ssh" {
-  type              = "ingress"
-  from_port         = 22
-  to_port           = 22
+  from_port         = 0
+  to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["75.152.59.245/32"]
   security_group_id = aws_security_group.bastion_sg.id
