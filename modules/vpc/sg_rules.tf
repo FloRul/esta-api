eb037cf07b4994c19f53da5c59406c5b351c99ba
FloRul <rumiel.florian@gmail.com>
2024-03-21 16:49:09 -0400

    Add ingress rule for local access to bastion security group

--- modules/vpc/sg_rules.tf
+++ modules/vpc/sg_rules.tf
@@ -43,6 +43,15 @@ resource "aws_security_group_rule" "bastion_sg_egress_ssm" {
   security_group_id        = aws_security_group.bastion_sg.id
 }
 
+resource "aws_security_group_rule" "bastion_sg_ingress_local" {
+  type              = "ingress"
+  from_port         = 5432
+  to_port           = 5432
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.bastion_sg.id
+}
+
 resource "aws_security_group_rule" "bastion_sg_egress_rds" {
   type                     = "egress"
   from_port                = 5432
