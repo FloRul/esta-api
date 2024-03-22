c675436cbcf34a787e4b9e3a52b92e2ea19b8f94
FloRul <rumiel.florian@gmail.com>
2024-03-22 11:34:53 -0400

    Add security group rules for bastion and database access***

--- modules/vpc/sg_rules.tf
+++ modules/vpc/sg_rules.tf
@@ -61,29 +61,32 @@ resource "aws_security_group_rule" "dynamo_db_sg_ingress" {
   security_group_id        = aws_security_group.dynamo_db_sg.id
 }
 
-resource "aws_security_group_rule" "bastion_to_rds" {
+# Allow traffic from the bastion security group to the RDS security group
+resource "aws_security_group_rule" "allow_bastion_to_database" {
   type                     = "ingress"
   from_port                = 5432
   to_port                  = 5432
   protocol                 = "tcp"
-  security_group_id        = aws_security_group.bastion_sg.id
+  security_group_id        = aws_security_group.database_sg.id
   source_security_group_id = aws_security_group.bastion_sg.id
 }
 
-resource "aws_security_group_rule" "ssm_to_bastion" {
+# Allow SSM traffic to the bastion security group
+resource "aws_security_group_rule" "allow_ssm_to_bastion" {
   type                     = "ingress"
-  from_port                = 22
-  to_port                  = 22
+  from_port                = 443
+  to_port                  = 443
   protocol                 = "tcp"
   security_group_id        = aws_security_group.bastion_sg.id
   source_security_group_id = aws_security_group.ssm_sg.id
 }
 
-resource "aws_security_group_rule" "bastion_outbound" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  security_group_id = aws_security_group.bastion_sg.id
-  cidr_blocks       = ["0.0.0.0/0"]
+# Allow egress traffic from the bastion security group to the SSM security group
+resource "aws_security_group_rule" "allow_bastion_to_ssm" {
+  type                     = "egress"
+  from_port                = 443
+  to_port                  = 443
+  protocol                 = "tcp"
+  security_group_id        = aws_security_group.bastion_sg.id
+  source_security_group_id = aws_security_group.ssm_sg.id
 }
