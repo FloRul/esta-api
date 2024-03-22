7e048c6fafda9ea5cccc50a15922932017b61998
FloRul <rumiel.florian@gmail.com>
2024-03-22 10:32:04 -0400

    Add PostgreSQL installation to bastion.tf

--- modules/vectorstore/bastion.tf
+++ modules/vectorstore/bastion.tf
@@ -32,6 +32,7 @@ resource "aws_instance" "bastion" {
 sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
 sudo systemctl start amazon-ssm-agent
 sudo systemctl enable amazon-ssm-agent
+sudo yum install -y postgresql
 EOF
 }
 
